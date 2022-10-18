d8bf324f945bab571d771f52eb7678f40b0566fe
cluck135 <casenluck@gmail.com>
2022-10-18 15:12:07 -0400

    made animal spawning random

--- Assets/Scripts/AnimalSpawner.cs
+++ Assets/Scripts/AnimalSpawner.cs
@@ -10,15 +10,23 @@ public class AnimalSpawner : MonoBehaviour
     private float spawnPostionZ = 29;
     private float spawnInterval = 1.5f;
     private float startDelay = 2;
+    private float targetTime;
+
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        targetTime = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0)
+        {
+            SpawnRandomAnimal();
+            targetTime = Random.Range(0, 5);
+            //Debug.Log(targetTime);
+        }
     }
     void SpawnRandomAnimal() {
         int animalIndex = Random.Range(0, animalsPrefabs.Length);
